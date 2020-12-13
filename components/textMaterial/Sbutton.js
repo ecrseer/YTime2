a9b113fa8a7b32e9545869cbd531dd9e19c0ae59
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-13 12:35:34 -0300

    Discovered: props cant be pass as parameter out of function

--- components/textMaterial/Sbutton.js
+++ components/textMaterial/Sbutton.js
@@ -1,12 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-
-
-async function copiar(urlMod){
-    navigator.clipboard.writeText(''+urlMod).then(()=>{})
-    .catch(err=>console.log(err));
-}
-
+import { useEffect, useState } from 'react';
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
@@ -17,7 +11,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Sbutton({urlModif}) {
     const classes = useStyles();
-
+    const [myNav,setMyNav] = useState('');
+    useEffect(()=>{
+        setMyNav(navigator.share)
+    },[])
+    async function copiar(urlMod){
+        
+        navigator.clipboard.writeText('test'+urlMod).then(()=>{})
+        .catch(err=>console.log(err));
+    }
      async function compartilhar(urlModi){        
         const shareData = {           
             title:''+urlModi,
@@ -32,7 +34,7 @@ export default function Sbutton({urlModif}) {
     return (
       <div className={classes.root}>
              
-        {typeof navigator === "undefined"?
+        {myNav !== ''?
             <Button variant="contained" color="primary"
          onClick={()=>{copiar(urlModif)}} >
             Copiar
