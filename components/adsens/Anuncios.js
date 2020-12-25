ece84ea706468c337592e4edaf6d01e7b2ddcab6
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-24 23:33:27 -0300

    ads2

--- components/adsens/Anuncios.js
+++ components/adsens/Anuncios.js
@@ -1,13 +1,17 @@
-import {repelent} from '../../assets/ads';
-const Anuncios = ({esconde})=>{
+import { repelent } from '../../assets/ads';
+const Anuncios = ({ esconde }) => {
 
-    return(
-    <div>
-    <iframe style={{width:'120px',height:'242px'}} scrolling="no" frameBorder="0"
- src={repelent}>
-</iframe>
-<p>{esconde}</p>
-
-</div>)}
+    return (
+        <div>
+            <iframe style={{ width: '120px', height: '242px' }} scrolling="no" frameBorder="0"
+                src={repelent}>
+            </iframe>
+            
+            <iframe style="width:120px;height:240px;" marginWidth="0" marginHeight="0" scrolling="no" 
+            frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=BR&source=ac&ref=tf_til&ad_type=product_link&tracking_id=ecrseer0d-20&marketplace=amazon&region=BR&placement=B08PCDQJCV&asins=B08PCDQJCV&linkId=4f939e4fbb69b9cc32f3866d97231469&show_border=false&link_opens_in_new_window=true&price_color=d6d5c9&title_color=ffffff&bg_color=b53369">
+    </iframe>
+            <a target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=ecrseer0d-20&linkCode=ur2&linkId=fa804767afc41de55fb4eb8f04918502&camp=1789&creative=9325&index=computers&keywords=gtx">nvdia info</a><img src="//ir-br.amazon-adsystem.com/e/ir?t=ecrseer0d-20&l=ur2&o=33" width="1" height="1" border="0" alt="" style={{border:'none !important', margin:'0px !important',}} />
+        </div>)
+}
 
 export default Anuncios;
