df6ada5b6df0686f0f7ace8978b6892f5e094b88
ecrseer <juustino@live.com>
2021-01-16 14:04:41 -0300

    add: campo para url

--- components/layout/Topbar.js
+++ components/layout/Topbar.js
@@ -7,21 +7,30 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { stylex } from './Topbarcss';
-import { Box } from '@material-ui/core';
+import { Box, TextField } from '@material-ui/core';
 
 const useStyles = makeStyles(stylex);
 
-export default function ButtonAppBar() {
+export default function ButtonAppBar({NoCopy,setarNoCopyURL}) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.toolbar}>
-      
-        <Toolbar className={classes.toolbar}>          
-          <Typography variant="h6" className={classes.titl}>
+      <Toolbar className={classes.toolbar}>    
+       
+        {!NoCopy?
+            <Typography variant="h6" className={classes.titl}>
             Time Shared for Youtube
-          </Typography>          
+          </Typography>  
+          :
+          <TextField
+                      id="standard-basic" name="min"                
+                      label="URL" onChange={(ev)=>{
+                        setarNoCopyURL(
+                        ev.target.value)
+                      }} fullWidth style={{margin:'1.5%'}} />
+        }
         </Toolbar>
         
       </AppBar>
